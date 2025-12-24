471ba4d948f197ef84f694fd22325be32621f5ed
tariful6 <tariful2306@gmail.com>
2025-12-24 15:24:29 +0600

    test3

--- src/actions/subscription.ts
+++ src/actions/subscription.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 import { checkSubscription } from './course';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-10-29.clover',
+  apiVersion: '2025-10-29.clover', 
 });
 
 /**
