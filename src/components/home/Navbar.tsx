d92ee87dde2681de627b5dc737746ed3c63f698e
tariful6 <tariful2306@gmail.com>
2025-12-25 22:56:01 +0600

    responsive navber

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -4,18 +4,16 @@ const Navbar = () => {
     return (
         <div className=' bg-black text-white py-6'>
             <div className='px-[10%] container mx-auto flex justify-between items-center'>
-               <div className=' text-2xl font-bold cursor-pointer '>LMS TUTOR</div>
+               <div className='md:text-2xl md:font-bold text-xl cursor-pointer'>LMS TUTOR</div>
                <div>
-                    <ul className=' flex items-center gap-6'>
+                    <ul className='md:flex items-center gap-6 hidden'>
                         <li className=' text-md cursor-pointer hover:text-yellow-400 pb-1 border-b-2 border-b-transparent  hover:border-amber-500'>Home</li>
                         <li className=' text-md cursor-pointer hover:text-yellow-400 pb-1 border-b-2 border-b-transparent  hover:border-amber-500'>Course</li>
-                        <li className=' text-md cursor-pointer hover:text-yellow-400 pb-1 border-b-2 border-b-transparent  hover:border-amber-500'>Galary</li>
-                        <li className=' text-md cursor-pointer hover:text-yellow-400 pb-1 border-b-2 border-b-transparent  hover:border-amber-500'>Contract</li>
+                        <li className=' text-md cursor-pointer hover:text-yellow-400 pb-1 border-b-2 border-b-transparent  hover:border-amber-500'>about</li>
                     </ul>
                </div>
             </div>
         </div>
     );
 };
-
 export default Navbar;
